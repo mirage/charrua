a6af0742154122ddf5095f9a774f810b9186547a
Christiano F. Haesbaert <haesbaert@haesbaert.org>
2015-03-30 21:43:24 +0200

    Simplify build.sh and move stuff to _tags

--- build.sh
+++ build.sh
@@ -1,15 +1,3 @@
 #!/bin/sh
 
-ocamlbuild \
-	-use-ocamlfind \
-	-pkg cmdliner \
-	-pkg lwt \
-	-pkg lwt.unix \
-	-pkg lwt.log \
-	-pkg lwt.syntax \
-	-tag 'syntax(camlp4o)' \
-	-tag debug \
-	-tag bin_annot \
-	-cflags "-w A-4-33-40-41-42-43-34-44" \
-	-cflags -strict-sequence \
-	hdhcpd.byte
+ocamlbuild -use-ocamlfind hdhcpd.byte
