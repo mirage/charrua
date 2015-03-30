b1f0a0a6a3201b258227b655a100873d5f7d01d3
Christiano F. Haesbaert <haesbaert@haesbaert.org>
2015-03-31 01:23:02 +0200

    Simplify verbosity parsing, I seem to be getting smarter.

--- hdhcpd.ml
+++ hdhcpd.ml
@@ -1,31 +1,21 @@
 open Lwt
 
-let config_log verbosity =
-  match verbosity with
-  | None -> `Ok ()
-  | Some verbosity ->
-    let vlevel = match verbosity with
-      | "debug" -> Some Lwt_log.Debug
-      | "info" -> Some Lwt_log.Info
-      | "notice" -> Some Lwt_log.Notice
-      | _ -> None
-    in match vlevel with
-    | Some vlevel -> Lwt_log.add_rule "*" vlevel; `Ok ()
-    | None -> `Error (true, "invalid verbosity")
+let config_log = function
+  | "debug" -> Lwt_log.add_rule "*" Lwt_log.Debug
+  | "info" -> Lwt_log.add_rule "*" Lwt_log.Info
+  | "notice" -> Lwt_log.add_rule "*" Lwt_log.Notice
+  | _ -> invalid_arg "Verbosity should be debug|info|notice"
 
 let hdhcpd verbosity =
-  match (config_log verbosity) with
-  | `Error (t, e) -> `Error (t, e)
-  | _ -> Lwt_main.run(
-      Lwt_log.notice "Haesbaert DHCPD started" >>= fun () ->
-      Lwt_log.notice "Haesbaert DHCPD finished" >>= fun () ->
-      Lwt.return (`Ok ()))
+  let () = config_log verbosity in
+  let () = Lwt_log.ign_notice "Haesbaert DHCPD started" in
+  Lwt_main.run (Lwt_log.notice "Haesbaert DHCPD finished")
 
 (* Parse command line and start the ball *)
 open Cmdliner
 let cmd =
-  let verbosity = Arg.(value & opt (some string) None & info ["v" ; "verbosity"]
+  let verbosity = Arg.(value & opt string "notice" & info ["v" ; "verbosity"]
                          ~doc:"Log verbosity, debug|info|notice") in
-  Term.(ret (pure hdhcpd $ verbosity)),
+  Term.(pure hdhcpd $ verbosity),
   Term.info "hdhcpd" ~version:"0.1" ~doc:"Haesbaert DHCP"
 let () = match Term.eval cmd with `Error _ -> exit 1 | _ -> exit 0
