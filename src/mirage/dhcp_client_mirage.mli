b550311b47586667d9488e0b65b37ec6f3336ab4
David Sheets <sheets@alum.mit.edu>
2017-01-15 17:21:07 +0000

    Update to build with mirage-dev

--- src/mirage/dhcp_client_mirage.mli
+++ src/mirage/dhcp_client_mirage.mli
@@ -1,4 +1,5 @@
-val ipv4_config_of_lease : Dhcp_wire.pkt -> Mirage_types_lwt.ipv4_config option
+val ipv4_config_of_lease :
+  Dhcp_wire.pkt -> Mirage_protocols_lwt.ipv4_config option
 (** [ipv4_config_of_lease pkt] checks whether [pkt] represents
  *  a valid IPv4 configuration (regardless of the time at which
  *  [pkt] was sent and the time given for its validity).  If [pkt]
@@ -8,7 +9,7 @@ val ipv4_config_of_lease : Dhcp_wire.pkt -> Mirage_types_lwt.ipv4_config option
  *  IPv4 configuration, [ipv4_config_of_lease pkt] returns [None]. *)
 
 module Make(Time : Mirage_types_lwt.TIME) (Network : Mirage_types_lwt.NETWORK) : sig
-  type t = Mirage_types_lwt.ipv4_config Lwt_stream.t
+  type t = Mirage_protocols_lwt.ipv4_config Lwt_stream.t
   val connect : ?requests:Dhcp_wire.option_code list
     -> Network.t -> t Lwt.t
   (** [connect ?requests net] attempts to use [net] to obtain a valid
--- src/mirage/dhcp_ipv4.mli
+++ src/mirage/dhcp_ipv4.mli
@@ -14,7 +14,7 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *)
 
-module Make (D: Mirage_types_lwt.DHCP_CLIENT) (E:Mirage_types_lwt.ETHIF) (A: Mirage_types_lwt.ARP) : sig
+module Make (D: Mirage_protocols_lwt.DHCP_CLIENT) (E:Mirage_types_lwt.ETHIF) (A: Mirage_types_lwt.ARP) : sig
   include Mirage_types_lwt.IPV4 with type ethif = E.t
   val connect : D.t -> ethif -> A.t -> t Lwt.t
     (** Connect to an ipv4 device using information from a DHCP lease. *)
