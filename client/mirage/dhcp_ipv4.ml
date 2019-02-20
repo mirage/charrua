50e8d4ffc9ce8dcaa25e61d718e46194322e999d
Hannes Mehnert <hannes@mehnert.org>
2019-02-20 14:28:58 +0100

    rename ethif to ethernet

--- client/mirage/dhcp_ipv4.ml
+++ client/mirage/dhcp_ipv4.ml
@@ -1,10 +1,10 @@
 open Lwt.Infix
 open Mirage_protocols_lwt
 
-module Make(Dhcp_client : DHCP_CLIENT) (R : Mirage_random.C) (C : Mirage_clock.MCLOCK) (Ethif : ETHIF)(Arp : ARP) = struct
+module Make(Dhcp_client : DHCP_CLIENT) (R : Mirage_random.C) (C : Mirage_clock.MCLOCK) (E : ETHERNET) (Arp : ARP) = struct
   (* for now, just wrap a static ipv4 *)
-  include Static_ipv4.Make(R)(C)(Ethif)(Arp)
-  let connect dhcp clock ethif arp =
+  include Static_ipv4.Make(R)(C)(E)(Arp)
+  let connect dhcp clock ethernet arp =
     Lwt_stream.last_new dhcp >>= fun (config : ipv4_config) ->
-    connect ~ip:config.address ~network:config.network ~gateway:config.gateway clock ethif arp
+    connect ~ip:config.address ~network:config.network ~gateway:config.gateway clock ethernet arp
 end
